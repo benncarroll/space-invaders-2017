9a87fc456e782b600c4ba777b01ef70fe2182f68
Peter Howse <p.howse@tsc.nsw.edu.au>
2017-02-17 10:31:31 +1100

    Initial commit
    - Aliens
    - Ship
    - Bullets
    - Shells
    - Checking for hits

---
+++ SpaceInvaders2017/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SpaceInvaders2017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrGame = new System.Windows.Forms.Timer(this.components);
+            this.tmrSlow = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrGame
+            // 
+            this.tmrGame.Enabled = true;
+            this.tmrGame.Tick += new System.EventHandler(this.tmrGame_Tick);
+            // 
+            // tmrSlow
+            // 
+            this.tmrSlow.Enabled = true;
+            this.tmrSlow.Interval = 500;
+            this.tmrSlow.Tick += new System.EventHandler(this.tmrSlow_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(869, 553);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrGame;
+        private System.Windows.Forms.Timer tmrSlow;
+    }
+}
+
---
+++ SpaceInvaders2017/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceInvaders2017
+{
+    public partial class Form1 : Form
+    {
+        
+        List<PictureBox> arrAliens = new List<PictureBox>();
+        bool moveDir = true;
+        int shipDir = 0;
+        PictureBox ship = new PictureBox();
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPress += Form1_KeyPress;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar.ToString() == " ")
+            {
+                createBullet();
+            }
+
+            if(e.KeyChar.ToString() == "a")
+            {
+                shipDir = -20;
+            }
+            else if(e.KeyChar.ToString() == "d")
+            {
+                shipDir = 20;
+            }
+            else if(e.KeyChar.ToString() == "s")
+            {
+                shipDir = 0;
+            }
+        }
+
+        private void moveShip()
+        {
+            ship.Left += shipDir;
+
+            if(ship.Left > this.Width)
+            {
+                ship.Left = -ship.Width;
+            }
+            else if(ship.Left < -ship.Width)
+            {
+                ship.Left = this.Width;
+            }
+        }
+
+        private void createBullet()
+        {
+            PictureBox shell = new PictureBox();
+            shell.Size = new Size(3, 5);
+            shell.Location = new Point(ship.Left + (ship.Width / 2), ship.Top);
+            shell.Name = "shell";
+            shell.BackColor = Color.DarkOrange;
+            Controls.Add(shell);
+        }
+
+        private void moveBullet()
+        {
+            foreach (PictureBox p in Controls)
+            {
+                if (p.Name == "shell") { 
+                    if (p.Top < 0)
+                    {
+                        Controls.Remove(p);
+                    }
+                    else
+                    {
+                        p.Top -= 20;
+                    }
+                }
+                if (p.Name == "bullet")
+                {
+                    if (p.Top < 0)
+                    {
+                        Controls.Remove(p);
+                    }
+                    else
+                    {
+                        p.Top += 20;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createShip();
+            ship = (PictureBox)Controls["ship"];
+            //ship.Left += 500;
+            createAliens();
+        }
+
+        private void createShip()
+        {
+            PictureBox ship = new PictureBox();
+            ship.Size = new Size(20, 20);
+            ship.BackColor = Color.Red;
+            ship.Name = "ship";
+            ship.Location = new Point(this.Width / 2, this.Height - 100);
+            Controls.Add(ship);
+
+        }
+
+        private void createAliens()
+        {
+            for(int y = 0; y<=2; y++)
+            {
+                for(int x = 0; x <= 9; x++)
+                {
+                    PictureBox pic = new PictureBox();
+                    pic.Size = new Size(20,20);
+                    pic.Location = new Point(50 + 30 * x, 50 + 30 * y);
+                    pic.BackColor = Color.LawnGreen;
+                    pic.Name = "alien";
+                    Controls.Add(pic);
+                    arrAliens.Add(pic);
+                }
+            }
+        }
+
+        private void tmrGame_Tick(object sender, EventArgs e)
+        {
+            moveAliens();
+            moveBullet();
+            moveShip();
+            checkHits();
+        }
+
+        private void moveAliens()
+        {
+            foreach(PictureBox alien in arrAliens)
+            {
+                if (moveDir == true)
+                {
+                    alien.Left += 10;
+                }
+                else
+                {
+                    alien.Left -= 10;
+                }
+                
+                if(alien.Left >= this.Width - 21)
+                {
+                    moveDir = false;
+                }
+                else if (alien.Left <= 21)
+                {
+                    moveDir = true;
+                }
+                
+            }
+        }
+
+        private void checkHits()
+        {
+            foreach (PictureBox a in arrAliens)
+            {
+                foreach (PictureBox p in Controls.OfType<PictureBox>())
+                {
+               
+                    if (p.Name == "shell" && p.Bounds.IntersectsWith(a.Bounds))
+                    {
+                        
+                        p.BackColor = this.BackColor;
+                        a.BackColor = this.BackColor;
+                        p.Tag = "dead";
+                        a.Tag = "dead";
+                    }
+                }
+            }
+
+            foreach(PictureBox p in Controls)
+            {
+                if(p.Tag ==  "dead")
+                {
+                    Controls.Remove(p);
+
+                    if(p.Name == "alien")
+                    {
+                        arrAliens.Remove(p);
+                    }
+                }
+            }
+
+        }
+
+        private void tmrSlow_Tick(object sender, EventArgs e)
+        {
+            shootAtShip();
+        }
+
+        private void shootAtShip()
+        {
+            int x = r.Next(0, arrAliens.Count);
+
+            PictureBox pic = new PictureBox();
+            pic.Size = new Size(3, 5);
+            pic.BackColor = Color.YellowGreen;
+            pic.Location = new Point(arrAliens[x].Left + (arrAliens[x].Width / 2), arrAliens[x].Bottom);
+            pic.Name = "bullet";
+            Controls.Add(pic);
+        }
+
+    }
+}
