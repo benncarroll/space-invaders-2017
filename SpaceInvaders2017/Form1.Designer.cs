d545de10f5cb5e45a7ece41a5dd9f8643d2d480d
Ben Carroll <Ben Carroll>
2017-02-22 17:28:55 +1100

    Massive commit, first commit. +Waves, with more aliens each time +End game screen, with option to spawn next wave +Ship update timer, keeping it at the bottom of the screen +Changed createAliens function to allow for custom amount of aliens +Fixed alien movement glitch where alignment is messed up +Added alien movement down screen toward ship +Added speed-up timer in relation to alien movement

--- SpaceInvaders2017/Form1.Designer.cs
+++ SpaceInvaders2017/Form1.Designer.cs
@@ -29,8 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tmrGame = new System.Windows.Forms.Timer(this.components);
             this.tmrSlow = new System.Windows.Forms.Timer(this.components);
+            this.gameProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.displayText = new System.Windows.Forms.Label();
+            this.tmrMove = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // tmrGame
@@ -44,14 +48,40 @@
             this.tmrSlow.Interval = 500;
             this.tmrSlow.Tick += new System.EventHandler(this.tmrSlow_Tick);
             // 
+            // gameProgress
+            // 
+            this.gameProgress.Name = "gameProgress";
+            this.gameProgress.Size = new System.Drawing.Size(100, 15);
+            // 
+            // displayText
+            // 
+            this.displayText.BackColor = System.Drawing.Color.Transparent;
+            this.displayText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.displayText.Font = new System.Drawing.Font("NSimSun", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.displayText.Location = new System.Drawing.Point(0, 0);
+            this.displayText.Name = "displayText";
+            this.displayText.Size = new System.Drawing.Size(869, 553);
+            this.displayText.TabIndex = 0;
+            this.displayText.Text = "placeholder";
+            this.displayText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.displayText.Visible = false;
+            // 
+            // tmrMove
+            // 
+            this.tmrMove.Enabled = true;
+            this.tmrMove.Tick += new System.EventHandler(this.tmrMove_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(869, 553);
+            this.Controls.Add(this.displayText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Space Invaders!";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -61,6 +91,9 @@
 
         private System.Windows.Forms.Timer tmrGame;
         private System.Windows.Forms.Timer tmrSlow;
+        private System.Windows.Forms.ToolStripProgressBar gameProgress;
+        private System.Windows.Forms.Label displayText;
+        private System.Windows.Forms.Timer tmrMove;
     }
 }
 
--- SpaceInvaders2017/Form1.cs
+++ SpaceInvaders2017/Form1.cs
@@ -15,6 +15,9 @@ namespace SpaceInvaders2017
         
         List<PictureBox> arrAliens = new List<PictureBox>();
         bool moveDir = true;
+        bool finished = false;
+        int aliensx = 3;
+        int aliensy = 10;
         int shipDir = 0;
         PictureBox ship = new PictureBox();
         Random r = new Random();
@@ -27,7 +30,7 @@ namespace SpaceInvaders2017
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if(e.KeyChar.ToString() == " ")
+            if(e.KeyChar.ToString() == "w")
             {
                 createBullet();
             }
@@ -44,11 +47,37 @@ namespace SpaceInvaders2017
             {
                 shipDir = 0;
             }
+
+            if (e.KeyChar.ToString() == "n")
+            {
+                if (finished)
+                {
+                    newWave();
+                }
+            }
+            if (e.KeyChar.ToString() == "")
+            {
+
+            }
+
+
         }
+
+        //private void compareKeys(KeyPressEventArgs a, string b)
+        //{
+        //    if (a.KeyChar.ToString() == b)
+        //    {
+        //        return true;
+        //    } else
+        //    {
+        //        return false;
+        //    }
+        //}
 
         private void moveShip()
         {
             ship.Left += shipDir;
+
 
             if(ship.Left > this.Width)
             {
@@ -58,6 +87,9 @@ namespace SpaceInvaders2017
             {
                 ship.Left = this.Width;
             }
+
+            ship.Location = new Point(ship.Left, (this.Height - 100));
+
         }
 
         private void createBullet()
@@ -72,7 +104,7 @@ namespace SpaceInvaders2017
 
         private void moveBullet()
         {
-            foreach (PictureBox p in Controls)
+            foreach (PictureBox p in Controls.OfType<PictureBox>())
             {
                 if (p.Name == "shell") { 
                     if (p.Top < 0)
@@ -103,7 +135,7 @@ namespace SpaceInvaders2017
             createShip();
             ship = (PictureBox)Controls["ship"];
             //ship.Left += 500;
-            createAliens();
+            createAliens(3,10);
         }
 
         private void createShip()
@@ -111,17 +143,18 @@ namespace SpaceInvaders2017
             PictureBox ship = new PictureBox();
             ship.Size = new Size(20, 20);
             ship.BackColor = Color.Red;
+            ship.ImageLocation = @"C:\Users\Ben Carroll\BitBucket\Ben's Space Invaders\SpaceInvaders2017\img\alien.ico";
             ship.Name = "ship";
             ship.Location = new Point(this.Width / 2, this.Height - 100);
             Controls.Add(ship);
 
         }
 
-        private void createAliens()
+        private void createAliens(int f, int g)
         {
-            for(int y = 0; y<=2; y++)
+            for(int y = 0; y < f; y++)
             {
-                for(int x = 0; x <= 9; x++)
+                for(int x = 0; x < g; x++)
                 {
                     PictureBox pic = new PictureBox();
                     pic.Size = new Size(20,20);
@@ -136,15 +169,15 @@ namespace SpaceInvaders2017
 
         private void tmrGame_Tick(object sender, EventArgs e)
         {
-            moveAliens();
-            moveBullet();
+            //moveAliens();
+            //moveBullet();
             moveShip();
             checkHits();
         }
 
         private void moveAliens()
         {
-            foreach(PictureBox alien in arrAliens)
+            foreach (PictureBox alien in arrAliens)
             {
                 if (moveDir == true)
                 {
@@ -154,17 +187,38 @@ namespace SpaceInvaders2017
                 {
                     alien.Left -= 10;
                 }
-                
-                if(alien.Left >= this.Width - 21)
+            }
+
+            foreach (PictureBox alien in arrAliens)
+            {
+
+                if (alien.Left >= this.Width - 21)
                 {
                     moveDir = false;
+                    alienUpdate();
                 }
+                
                 else if (alien.Left <= 21)
                 {
                     moveDir = true;
+                    alienUpdate();
                 }
-                
             }
+
+            }
+
+        private void alienUpdate()
+        {
+            foreach (PictureBox a in arrAliens)
+            {
+                a.Top += 10;
+            }
+
+            if (tmrMove.Interval > 40)
+            {
+                tmrMove.Interval -= 10;
+            }
+
         }
 
         private void checkHits()
@@ -176,7 +230,6 @@ namespace SpaceInvaders2017
                
                     if (p.Name == "shell" && p.Bounds.IntersectsWith(a.Bounds))
                     {
-                        
                         p.BackColor = this.BackColor;
                         a.BackColor = this.BackColor;
                         p.Tag = "dead";
@@ -185,9 +238,11 @@ namespace SpaceInvaders2017
                 }
             }
 
-            foreach(PictureBox p in Controls)
+            foreach(PictureBox p in Controls.OfType<PictureBox>())
             {
-                if(p.Tag ==  "dead")
+#pragma warning disable CS0252 // Possible unintended reference comparison; left hand side needs cast
+                if (p.Tag ==  "dead")
+#pragma warning restore CS0252 // Possible unintended reference comparison; left hand side needs cast
                 {
                     Controls.Remove(p);
 
@@ -202,19 +257,63 @@ namespace SpaceInvaders2017
 
         private void tmrSlow_Tick(object sender, EventArgs e)
         {
+            checkEndGame();
             shootAtShip();
+        }
+
+        private void checkEndGame()
+        {
+
+            if (arrAliens.Count < 1 && !finished)
+            {
+
+                finished = true;
+
+                aliensx++;
+                aliensy++;
+
+                setText("PRESS N TO START NEXT WAVE\nINVADERS: " + (aliensx * aliensy));
+            }
+        }
+
+        private void setText(string a)
+        {
+            displayText.Text = a;
+            displayText.Visible = true;
+        }
+
+        private void newWave()
+        {
+            displayText.Visible = false;
+            finished = false;
+
+            createAliens(aliensx,aliensy);
+
+            //tmrGame.Enabled = true;
+            //tmrSlow.Enabled = true;
         }
 
         private void shootAtShip()
         {
-            int x = r.Next(0, arrAliens.Count);
+            if (finished == false)
+            {
+                int x = r.Next(0, arrAliens.Count);
 
-            PictureBox pic = new PictureBox();
-            pic.Size = new Size(3, 5);
-            pic.BackColor = Color.YellowGreen;
-            pic.Location = new Point(arrAliens[x].Left + (arrAliens[x].Width / 2), arrAliens[x].Bottom);
-            pic.Name = "bullet";
-            Controls.Add(pic);
+                PictureBox pic = new PictureBox();
+                pic.Size = new Size(3, 5);
+                pic.BackColor = Color.YellowGreen;
+                pic.Location = new Point(arrAliens[x].Left + (arrAliens[x].Width / 2), arrAliens[x].Bottom);
+                pic.Name = "bullet";
+                Controls.Add(pic);
+            }
+        }
+
+        private void tmrMove_Tick(object sender, EventArgs e)
+        {
+                moveAliens();
+                moveBullet();
+                checkHits();
+            
         }
 
     }
